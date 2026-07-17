f8d607bf6ece75ac9fcb0bdd078617506918e337
Felix Otieno Ochieng <129052173+OCHIENG-FELIX@users.noreply.github.com>
2026-07-17 07:58:38 +0300

    Add SQL query for calculating monthly sales metrics

--- sql_queries/10_moving_average.sql
+++ sql_queries/10_moving_average.sql
@@ -19,3 +19,34 @@ Author: Felix Ochieng'
 Date: July 2026
 ==========================================================
 */
+WITH MonthlySales AS (
+SELECT
+strftime('%Y', o.Order_Date) AS Year,
+strftime('%m', o.Order_Date) AS Month,
+ROUND(SUM(od.Sales), 2) AS Total_Sales,
+ROUND(SUM(od.Profit), 2) AS Total_Profit,
+COUNT(DISTINCT o.Order_ID) AS Total_Orders
+FROM Orders o
+JOIN Order_Details od
+    ON o.Order_ID = od.Order_ID
+GROUP BY Year, Month
+ORDER BY Year, Month
+),
+MonthlyMetrics AS (
+SELECT *,
+LAG(Total_Sales) OVER (ORDER BY Year, Month) AS Previous_Month_Sales,
+AVG(Total_Sales)
+OVER (
+    ORDER BY Year, Month
+    ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+) AS Three_Month_Average_Sales
+FROM MonthlySales
+)
+SELECT*,
+ROUND(
+    (Total_Sales - Previous_Month_Sales) * 100.0 /
+    NULLIF(Previous_Month_Sales,0),
+    2
+) AS MoM_Sales_Growth_Percent
+FROM MonthlyMetrics
+ORDER BY Year, Month;
