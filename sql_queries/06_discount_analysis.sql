63c3af3f3d3f17bdf4be34159c1fe65e55977c97
Felix Otieno Ochieng <129052173+OCHIENG-FELIX@users.noreply.github.com>
2026-07-17 07:30:02 +0300

    Update 06_discount_analysis.sql

--- sql_queries/06_discount_analysis.sql
+++ sql_queries/06_discount_analysis.sql
@@ -1,1 +1,20 @@
-discount
+/*
+==========================================================
+Project : Superstore Sales Analytics
+File    : 06_discount_analysis.sql
+
+Description:
+Analyzes how discount levels influence company
+sales and profitability.
+
+Business Questions:
+1. How do discounts affect profit?
+2. Which discount levels remain profitable?
+3. At what discount level does the company begin
+   losing money?
+4. Should current discount policies be reviewed?
+
+Author: Felix Ochieng'
+Date: July 2026
+==========================================================
+*/
