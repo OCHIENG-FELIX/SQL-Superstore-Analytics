5daea4f20d7237a38cba7ee30d610a357257c97f
Felix Otieno Ochieng <129052173+OCHIENG-FELIX@users.noreply.github.com>
2026-07-17 07:50:36 +0300

    Add regional analysis SQL query for sales and profit

--- sql_queries/02_regional_analysis.sql
+++ sql_queries/02_regional_analysis.sql
@@ -17,3 +17,19 @@ Author: Felix Ochieng'
 Date: July 2026
 ==========================================================
 */
+
+SELECT
+    l.Region,
+    ROUND(SUM(od.Sales), 2) AS Total_Sales,
+    ROUND(SUM(od.Profit), 2) AS Total_Profit,
+    SUM(od.Quantity) AS Total_Quantity,
+    COUNT(DISTINCT o.Order_ID) AS Total_Orders,
+    ROUND((SUM(od.Profit) / SUM(od.Sales)) * 100, 2) AS Profit_Margin
+FROM Order_Details od
+JOIN Orders o
+    ON od.Order_ID = o.Order_ID
+JOIN Locations l
+    ON o.Location_Key = l.Location_Key
+GROUP BY l.Region
+ORDER BY Total_Sales DESC;
+
