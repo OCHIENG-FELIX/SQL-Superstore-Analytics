a4c8de593851060079c38e8eb7f816bfc67bd52e
Felix Otieno Ochieng <129052173+OCHIENG-FELIX@users.noreply.github.com>
2026-07-17 07:57:31 +0300

    Update 09_yoy_growth.sql

--- sql_queries/09_yoy_growth.sql
+++ sql_queries/09_yoy_growth.sql
@@ -17,3 +17,30 @@ Author: Felix Ochieng'
 Date: July 2026
 ==========================================================
 */
+
+WITH YearlySales AS (
+SELECT
+strftime('%Y', o.Order_Date) AS Year,
+ROUND(SUM(od.Sales), 2) AS Total_Sales,
+ROUND(SUM(od.Profit), 2) AS Total_Profit,
+COUNT(DISTINCT o.Order_ID) AS Total_Orders
+FROM Orders o
+JOIN Order_Details od
+    ON o.Order_ID = od.Order_ID
+GROUP BY Year
+ORDER BY Year
+),
+YearlyMetrics AS (
+SELECT *,
+LAG(Total_Sales) OVER (ORDER BY Year) AS Previous_Year_Sales
+FROM YearlySales
+)
+SELECT *,
+ROUND(
+    (Total_Sales - Previous_Year_Sales) * 100.0 /
+    NULLIF(Previous_Year_Sales, 0),
+    2
+) AS YoY_Sales_Growth_Percent
+FROM YearlyMetrics
+ORDER BY Year;
+
