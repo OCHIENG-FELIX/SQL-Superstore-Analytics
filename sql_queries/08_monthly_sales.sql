c9ad2ad6bd2476d5ad8e5a8a40e44868fd8c6f81
Felix Otieno Ochieng <129052173+OCHIENG-FELIX@users.noreply.github.com>
2026-07-17 07:29:09 +0300

    Add header comment to monthly sales SQL file
    Added header comment with project details and business questions.

--- sql_queries/08_monthly_sales.sql
+++ sql_queries/08_monthly_sales.sql
@@ -1,1 +1,19 @@
-monthly
+/*
+==========================================================
+Project : Superstore Sales Analytics
+File    : 08_monthly_sales.sql
+
+Description:
+Analyzes monthly business performance by calculating
+sales, profit, and total orders for each month.
+
+Business Questions:
+1. How do sales change from month to month?
+2. Which months generate the highest sales?
+3. Which months perform poorly?
+4. How does monthly business performance fluctuate?
+
+Author: Felix Ochieng'
+Date: July 2026
+==========================================================
+*/
