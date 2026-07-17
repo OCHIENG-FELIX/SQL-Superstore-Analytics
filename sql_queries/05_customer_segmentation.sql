eb764f3aa57b5648c83078f05ee313bdf343e0d2
Felix Otieno Ochieng <129052173+OCHIENG-FELIX@users.noreply.github.com>
2026-07-17 07:30:26 +0300

    Add header comment for customer segmentation SQL file
    Added a header comment with project details and business questions.

--- sql_queries/05_customer_segmentation.sql
+++ sql_queries/05_customer_segmentation.sql
@@ -1,1 +1,19 @@
-product
+/*
+==========================================================
+Project : Superstore Sales Analytics
+File    : 05_customer_segmentation.sql
+
+Description:
+Segments customers into business-defined groups
+using profitability metrics and NTILE().
+
+Business Questions:
+1. Which customers should be classified as VIP?
+2. Which customers belong to the Regular segment?
+3. Which customers require marketing attention?
+4. How should customer loyalty programs be designed?
+
+Author: Felix Ochieng'
+Date: July 2026
+==========================================================
+*/
