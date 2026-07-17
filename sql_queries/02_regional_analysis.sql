aa5cc18c72f42326a3fa33a583e0e88f2f60560c
Felix Otieno Ochieng <129052173+OCHIENG-FELIX@users.noreply.github.com>
2026-07-17 07:31:41 +0300

    Update 02_regional_analysis.sql

--- sql_queries/02_regional_analysis.sql
+++ sql_queries/02_regional_analysis.sql
@@ -1,1 +1,19 @@
-region
+/*
+==========================================================
+Project : Superstore Sales Analytics
+File    : 02_regional_analysis.sql
+
+Description:
+Analyzes business performance across different
+geographical regions.
+
+Business Questions:
+1. Which region generates the highest sales?
+2. Which region is the most profitable?
+3. Which region has the highest profit margin?
+4. Which region requires management attention?
+
+Author: Felix Ochieng'
+Date: July 2026
+==========================================================
+*/
